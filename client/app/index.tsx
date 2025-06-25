7f3d9966e6dd0cad3626488f2be27b9be5b78e5c
Alemayehu <alemayehudabi606@gmail.com>
2025-06-26 00:11:21 +0300

    FEAT: getting session from better

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,42 +1,63 @@
-"use client"
+"use client";
 
-import { useEffect } from "react"
-import { View, Text, Image } from "react-native"
-import { useRouter } from "expo-router"
-import { RobotAvatar } from "@/components/ui/RobotAvatar"
+import { useEffect, useState } from "react";
+import { View, Text, Image, Alert } from "react-native";
+import { useRouter } from "expo-router";
+import { authClient } from "@/lib/auth-client";
 
 export default function SplashScreen() {
-  const router = useRouter()
+  const router = useRouter();
+  const [session, setSession] = useState("");
+
+  // getting session form better auth
+  useEffect(() => {
+    const getSession = async () => {
+      const { data: session } = await authClient.getSession();
+      const token = session?.session.token;
+
+      if (!token) {
+        return Alert.alert("session has expire");
+      }
+      setSession(token);
+    };
+
+    getSession();
+  }, []);
 
   useEffect(() => {
     // Simulate app initialization
     const timer = setTimeout(() => {
       // TODO: Check if user is authenticated
-      const isAuthenticated = false // This should come from your auth state
+      const isAuthenticated = session; // This should come from your auth state
 
       if (isAuthenticated) {
-        router.replace("/chats")
+        router.replace("/chats");
       } else {
-        router.replace("/welcome")
+        router.replace("/welcome");
       }
-    }, 2000)
+    }, 2000);
 
-    return () => clearTimeout(timer)
-  }, [])
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <View className="flex-1 bg-white items-center justify-center px-6">
       <View className="flex-1 items-center justify-center">
-           <Image
-                  source={require("@/assets/images/logo.png")}
-                  style={{ width: 150, height: 150, resizeMode: "contain", marginBottom: 32 }}
-                  accessible
-                  accessibilityLabel="Fidel Logo"
-                />
-        
-            
-            <Text className="text-3xl font-bold text-gray-900 text-center mb-2">እንኳን ወደ ፊደል AI በደና መጡ! </Text>
-        
+        <Image
+          source={require("@/assets/images/logo.png")}
+          style={{
+            width: 150,
+            height: 150,
+            resizeMode: "contain",
+            marginBottom: 32,
+          }}
+          accessible
+          accessibilityLabel="Fidel Logo"
+        />
+
+        <Text className="text-3xl font-bold text-gray-900 text-center mb-2">
+          እንኳን ወደ ፊደል AI በደና መጡ!{" "}
+        </Text>
       </View>
 
       <View className="pb-8">
@@ -44,5 +65,5 @@ export default function SplashScreen() {
         <Text className="text-sm text-gray-500">Version 1.0</Text>
       </View>
     </View>
-  )
+  );
 }
